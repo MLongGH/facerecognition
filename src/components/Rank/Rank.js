a78d3e2cfc7872e3804501bb073d29033bafb42e
meilinglong <meilingl.usu@gmail.com>
2020-02-12 11:26:55 -0700

    use lambda on aws to list rank badge

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
-const Rank = ({name, entries}) => {
-  return (
-      <div>
-        <div className='white f3'>
-          {`${name}, your current entries is ...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: ''
+    }
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevProps.entries === this.props.entries && prevProps.name === this.props.name){
+      return null;
+    }
+
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://ntkpwr4mm0.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+        .then(response => response.json())
+        .then(data => this.setState({emoji: data.input}))
+        .catch(console.log)
+  }
+
+  render() {
+    return (
+        <div>
+          <div className='white f3'>
+            {`${this.props.name}, your current entries is ...`}
+          </div>
+          <div className='white f3'>
+            {this.props.entries}
+          </div>
+          <div className='white f5'>
+            {`Rank Badge: ${this.state.emoji}`}
+          </div>
         </div>
-        <div className='white f3'>
-          {entries}
-        </div>
-      </div>
-  );
-};
+    );
+  }
+}
 
 export default Rank;
