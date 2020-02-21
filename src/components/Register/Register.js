dad9b7dcf312af4a2f19b196ad62183cb045cecf
meilinglong <meilingl.usu@gmail.com>
2020-02-21 10:26:59 -0700

    fix bug on register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,6 +23,10 @@ class Register extends React.Component {
     this.setState({password: event.target.value})
   };
 
+  saveAuthTokenInSession = (token) => {
+    window.sessionStorage.setItem('token', token);
+  }
+
   onSubmitRegister = () => {
     fetch('https://damp-dusk-57532.herokuapp.com/register', {
       method: 'post',
@@ -36,6 +40,7 @@ class Register extends React.Component {
         .then(response=>response.json())
         .then(user => {
           if(user.id){
+            this.saveAuthTokenInSession(data.token);
             this.props.loadUser(user);
             this.props.onRouteChange('home');
           }
